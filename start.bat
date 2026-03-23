1a6785af545aea60c837ce2ed5bc8a028d8605c1
Your Name <your.email@example.com>
2026-03-23 11:41:34 +0500

    67

--- start.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Starting HelpDesk Application...
-start cmd /k "cd server && npx nodemon src/index.js"
-start cmd /k "cd client && npm run dev"
-echo Servers are starting!
-echo Frontend will be accessible at http://localhost:5173
-pause
